f221bb5994f2a697dffe8afa1eaf3d8f0267daa8
thirdzcee <thirdzcee@gmail.com>
2014-01-27 08:42:47 +0800

    update defconfig, restored 400mhz gpu clock but still retained 450mhz gpu OC, now gpu has 4 frequencies available, tweaked some other gpu settings that resulted in an improved 2d performance based on antutu benchmark

--- kernel/arch/arm/mach-msm/board-8930-gpu.c
+++ kernel/arch/arm/mach-msm/board-8930-gpu.c
@@ -118,6 +118,11 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		{
 			.gpu_freq = 450000000,
 			.bus_freq = 3,
+			.io_fraction = 0,
+		},
+		{
+			.gpu_freq = 400000000,
+			.bus_freq = 3,
 			.io_fraction = 10,
 		},
 		{
