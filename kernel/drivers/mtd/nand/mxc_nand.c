27ff746fd61cfddd651d6bd2073a790760523337
thirdzcee <thirdzcee@gmail.com>
2014-01-29 12:59:55 +0800

    release 3.0, patched kernel to 3.4.2 from linux.org

--- kernel/drivers/mtd/nand/mxc_nand.c
+++ kernel/drivers/mtd/nand/mxc_nand.c
@@ -1219,11 +1219,6 @@ static int __init mxcnd_probe(struct platform_device *pdev)
 	if (nfc_is_v21() && mtd->writesize == 4096)
 		this->ecc.layout = &nandv2_hw_eccoob_4k;
 
-	/* second phase scan */
-	if (nand_scan_tail(mtd)) {
-		err = -ENXIO;
-		goto escan;
-	}
 
 	if (this->ecc.mode == NAND_ECC_HW) {
 		if (nfc_is_v1())
@@ -1232,6 +1227,12 @@ static int __init mxcnd_probe(struct platform_device *pdev)
 			this->ecc.strength = (host->eccsize == 4) ? 4 : 8;
 	}
 
+	/* second phase scan */
+        if (nand_scan_tail(mtd)) {
+            err = -ENXIO;
+        goto escan;
+    }
+ 
 	/* Register the partitions */
 	mtd_device_parse_register(mtd, part_probes, NULL, pdata->parts,
 				  pdata->nr_parts);
